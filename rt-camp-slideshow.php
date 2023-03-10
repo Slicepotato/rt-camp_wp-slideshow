4ed52370a87d686df46064646661e5e9a2f5dbfd
Slicepotato <m.ianjohnson@gmail.com>
2023-03-10 16:50:51 -0500

    add styles and form image preview

--- rt-camp-slideshow.php
+++ rt-camp-slideshow.php
@@ -8,37 +8,65 @@
     * Author URI: https://www.codemjohnson.com/
     **/
 
-    /* Add Dashicons in WordPress Front-end */
-    
-    function load_dashicons_front_end() {
-        wp_enqueue_style( 'dashicons' );
-    }
-    add_action( 'wp_enqueue_scripts', 'load_dashicons_front_end' );
+    // Stylesheet inclusion
+    function utm_user_scripts() {
+        $plugin_url = plugin_dir_url( __FILE__ );
 
-    function rtcslide_options_page_html() {
-    ?>
+       wp_enqueue_style( 'style',  $plugin_url . "/style.css");
+    }
+    add_action( 'admin_print_styles', 'utm_user_scripts' );
+
+    function rtcslide_manage_page_html() { ?>
         <div class="wrap">
-        <h1><?php echo esc_html( get_admin_page_title() ); ?></h1>
-        <form action="options.php" method="post">
+            <h1><?php echo esc_html( get_admin_page_title() ); ?></h1>
+            <form id="upload_form" action=<?php plugin_dir_path( __FILE__ ) . "upload.php" ?> enctype="multipart/form-data" method="post" target="messages">
+            <p><input name="imgslide" id="upload_img" type="file" accept="image/*" onchange="loadFile(event)" /></p>
+            <ul id="imgPreviewContainer" class="img-preview">
+                <li><img id="imgPreview" width="200" /></li>
+                <li><button name="clear" type="reset" onclick="resetForm(event)">Clear</button></li>
+            </ul>
+            <hr />
             <?php
                 settings_fields( 'rtcamp_slideshow' );
                 do_settings_sections( 'rtcslide' );
                 submit_button( __( 'Save Settings', 'textdomain' ) );
             ?>
-        </form>
+            </form>
         </div>
-    <?php
+        <script>
+            var loadFile = function(event) {
+	            var image = document.getElementById('imgPreview');
+                var imgContainer = document.getElementById('imgPreviewContainer');
+                imgContainer.classList.add("show");
+	            image.src = URL.createObjectURL(event.target.files[0]);
+            };
+
+            var resetForm = function(event) {
+                var image = document.getElementById('imgPreview');
+                var imgContainer = document.getElementById('imgPreviewContainer');
+                image.src = '';
+                imgContainer.classList.remove("show");
+            }
+        </script>
+<?php
     }
-    add_action( 'admin_menu', 'rtc_slideshow_page' );
+    
     function rtc_slideshow_page() {
         add_menu_page(
             'rtCamp Slideshow',                                 // page title
             'rtCamp Slideshow',                                 // menu title
             'manage_options',                                   // capability
             'rtcslide',                                         // menu slug
-            'rtcslide_options_page_html',                       // callable
+            'rtcslide_manage_page_html',                        // callable
             'dashicons-slides',                                 // dashicon
             20                                                  // menu position/order
         );
     }
+    add_action( 'admin_menu', 'rtc_slideshow_page' );
+
+    /* Add Dashicons in WordPress Front-end */  
+    function load_dashicons_front_end() {
+        wp_enqueue_style( 'dashicons' );
+    }
+    add_action( 'wp_enqueue_scripts', 'load_dashicons_front_end' );
 ?>
